433a717a40b5c996185a69ccd27f55f21d5423b5
Mikhail Kraynov <kraynov@finch-melrose.com>
2016-01-14 14:48:24 +0300

    styles

---
+++ src/components/StyledComponent.js
@@ -0,0 +1,10 @@
+import React, {
+    Component
+} from 'react-native';
+import styles from '../lib/styles';
+
+class StyledComponent extends Component {
+
+};
+
+export default styles()(StyledComponent);
--- src/components/Test.js
+++ src/components/Test.js
@@ -3,12 +3,21 @@ import React, {
     View,
     Text,
 } from 'react-native';
+import StyledComponent from './StyledComponent';
 
-export default class Test extends Component {
+export default class Test extends StyledComponent {
+    static styles() {
+        return {
+            color: 'red',
+            fontSize: 10,
+            textAlign: 'left',
+        }
+    }
+
     render() {
         return (
             <View>
-                <Text style={{fontSize: 20, textAlign: 'center'}}>Welcome to React Native in Web!</Text>
+                <Text ref="text">Welcome to React Native in Web!</Text>
                 {this.props.children}
             </View>
         )
